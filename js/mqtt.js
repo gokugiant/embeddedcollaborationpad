c153a81918301095d7ce0c59e69e078e0e726f18
Florian Paproth <florian@paproth.biz>
2020-11-18 22:02:05 +0100

    Finish managing mqtt connection

--- js/mqtt.js
+++ js/mqtt.js
@@ -1,32 +1,56 @@
+var host;
 var mqtt;
 var reconnectTimeout = 2000;
+var connected_flag = 0;
 
 var mqttOnFailure = function(msg) {
 	console.log("Connection Attempt to Host "+host+"Failed");
-	setTimeout(MQTTconnect, reconnectTimeout);
+	setTimeout(connectMQTT, reconnectTimeout);
 }
 
 var mqttOnMessageArrived = function(msg){
 	out_msg="Message received "+msg.payloadString+"<br>";
 	out_msg=out_msg+"Message received Topic "+msg.destinationName;
 	console.log(out_msg);
-
 }
 
-var onConnect = function() {
+// call when the connection is lost
+var mqttOnConnectionLost = function(){
+	$('#disconnectmqttBtm').hide("slow").prop("disabled", true);
+	$('#connectmqttBtm').show().prop("disabled", false);
+	
+	console.log("connection lost");
+	connected_flag=0;
+	$("#mqttFieldset").prop("disabled", false);
+}
+
+var connectMQTT = function() {
 	$('#connectmqttBtm').hide("slow").prop("disabled", true);
-	$('#disconnectmqttBtm').show();
+	$('#disconnectmqttBtm').show().prop("disabled", false);
 	
 	// Once a connection has been made, make a subscription and send a message.
 	console.log("Connected ");
+	connected_flag=1;
 	mqtt.subscribe("sensor1");
 	message = new Paho.MQTT.Message("Hello World");
 	message.destinationName = "sensor1";
 	mqtt.send(message);
 }
 
-var mqttconnect = function() {
-	var host = $("#mqttServer").val(); 
+// Disconect from the MQTT Server
+var mqttDisconnect = function(){
+	console.log("Try to disconnect from the server");
+	if (connected_flag==1){
+		mqtt.disconnect();
+		onnected_flag=0;
+		$("#mqttFieldset").prop("disabled", false);
+	}else{
+		console.log("onnected_flag is not 1. Seems to be you are already disconnected");
+	}
+}
+
+var mqttConnect = function() {
+	host = $("#mqttServer").val(); 
 	var port = parseInt($("#mqttPort").val());
 	
 	console.log("connecting to "+ host +" "+ port);
@@ -36,13 +60,14 @@ var mqttconnect = function() {
 	
 	var options = {
 		timeout: 3,
-		onSuccess: onConnect,
+		onSuccess: connectMQTT,
 		onFailure: mqttOnFailure,
 		userName: $("#mqttUser").val(), 
 		password: $("#mqttPassword").val()
 	};
 	
-	mqtt.onMessageArrived = mqttOnMessageArrived
+	mqtt.onMessageArrived = mqttOnMessageArrived;
+	mqtt.onConnectionLost = mqttOnConnectionLost;
 	
 	mqtt.connect(options); //connect
 }
@@ -51,6 +76,15 @@ $(function() {
 	// Init connect button
 	$('#connectmqttBtm').click(function(){
 		$(this).prop("disabled", true);
-		mqttconnect();
+		$("#mqttFieldset").prop("disabled", true);
+		
+		mqttConnect();
 	});
+	
+	// Init disconect button
+	$('#disconnectmqttBtm').click(function(){
+		$(this).prop("disabled", true);
+		mqttDisconnect();
+	});
+	
 });
--- templates/mqtt.php
+++ templates/mqtt.php
@@ -1,23 +1,32 @@
 <div class="accordion" id="accordionSnipped">
-	<div class="form-row">
-	    <div class="col">
-	      <input id="mqttServer" type="text" name="server" class="form-control form-control-sm" value="hrw-fablab.de" readonly>
+	<div class="row">
+	    <div class="col-sm-8">
+			<fieldset id="mqttFieldset" class="form-group row">
+				<label for="mqttServer" class="col-sm-2 col-form-label">Server</label>
+				<div class="col-sm-10">
+					<input id="mqttServer" type="text" name="server" class="form-control form-control-sm" value="hrw-fablab.de">
+				</div>
+				<label for="mqttPort" class="col-sm-2 col-form-label">Port</label>
+				<div class="col-sm-10">
+					<input id="mqttPort" type="number" name="port" class="form-control form-control-sm" value="9001">
+				</div>
+				<label for="mqttUser" class="col-sm-2 col-form-label">Username</label>
+				<div class="col-sm-10">
+					<input id="mqttUser" type="text"  name="username" value="<?php echo $_COOKIE["mqttUser"]?>" name="port" class="form-control form-control-sm">
+				</div>
+				<label for="mqttPassword" class="col-sm-2 col-form-label">Password</label>
+				<div class="col-sm-10">
+					<input id="mqttPassword" type="text" name="password"  name="username" value="<?php echo $_COOKIE["mqttPwd"]?>" name="port" class="form-control form-control-sm">
+				</div>
+			</fieldset>
 	    </div>
-	    <div class="col">
-	      <input id="mqttPort" type="number" name="port" class="form-control form-control-sm" value="9001">
-	    </div>
-	    <div class="col">
-	      <input id="mqttUser" type="text"  name="username" value="<?php echo $_COOKIE["mqttUser"]?>" name="port" class="form-control form-control-sm">
-	    </div>
-	    <div class="col">
-	      <input id="mqttPassword" type="text" name="password"  name="username" value="<?php echo $_COOKIE["mqttPwd"]?>" name="port" class="form-control form-control-sm">
+	    <div class="col-sm-4">
+			<button id="connectmqttBtm" class="btn btn-success mqttControl"><i class="fas fa-link"></i> Connect</button>
+			<button id="disconnectmqttBtm" class="btn btn-danger mqttControl" style="display: none;"><i class="fas fa-times-circle"></i> Disconnect</button>
 	    </div>
 	</div>
 	
-	<button id="connectmqttBtm" class="btn btn-success"><i class="fas fa-link"></i> Connect</button>
-	<button id="disconnectmqttBtm" class="btn btn-danger" style="display: none;"><i class="fas fa-times-circle"></i> Disconnect</button>
-	
-	<hr class="my-4">
+	<hr>
 	
 	<div class="jumbotron">
 	  <div id="mqttConsole"></div>
